e9c472e33090581b4812c6e4ad8ecd299f02e6b1
Shubhankar1987 <103564533+Shubhankar1987@users.noreply.github.com>
2022-08-04 21:45:13 +0530

    SpringBootHelloWorldApplication.java
    Main class

---
+++ SpringBootHelloWorldApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+    }
+}
